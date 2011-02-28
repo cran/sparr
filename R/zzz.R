c0ef7053f478528d077dbce99d1085cd998185ac
T.M.D <t.davies@massey.ac.nz>
2011-02-28 00:00:00 +0000

    version 0.2-2

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 .onAttach <- function(libname, pkgname)
 {
-	packageStartupMessage("\nWelcome to 'sparr': SPAtial Relative Risk (v0.2-1)\nT.M. Davies, M.L. Hazelton & J.C. Marshall\ntype 'help(\"sparr\")' for details", appendLF=T)
+	packageStartupMessage("\nWelcome to 'sparr': SPAtial Relative Risk (v0.2-2)\nT.M. Davies, M.L. Hazelton & J.C. Marshall\ntype 'help(\"sparr\")' for details", appendLF=T)
 }
 
