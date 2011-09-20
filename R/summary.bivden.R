c099c37fa58ea672fdd9202fb6ba931372133c43
T.M.D <t.davies@massey.ac.nz>
2011-09-20 08:30:50 +0000

    version 0.2-3

--- R/summary.bivden.R
+++ R/summary.bivden.R
@@ -6,8 +6,12 @@ summary.bivden <- function(object, ...){
 
 	cat("Evaluated over",nrow(object$Zm),"by",ncol(object$Zm),"rectangular grid.\nDefined study region is a polygon with",length(vertices(object$WIN)$x),"vertices.\n\n")
 	
-	cat("Estimated density description\n")
-	print(summary(as.vector(object$Zm)))
+	#cat("Estimated density description\n")
+	
+	cat("Estimated density range ",min(as.vector(object$Zm),na.rm=T)," to ",max(as.vector(object$Zm),na.rm=T),".\n",sep="")
+	cat(sum(!is.na(as.vector(object$Zm))),"grid cells out of",prod(dim(object$Zm)),"fall inside study region.\n")
+	
+	#print(summary(as.vector(object$Zm)))
 
 }
 	
